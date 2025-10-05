7680ead72c8f6169c782237d298f6e7d198d7ed6
DarlingTub24 <150879694+DarlingTub24@users.noreply.github.com>
2025-10-04 21:52:27 -0700

    Delete Unidad 2 IA directory

--- Unidad 2 IA/Sistema_Recomendacion_Peliculas/Sistema_Recomendacion_Peliculas/actores.py
+++
@@ -1,7 +0,0 @@
-##Constructor de objetos actores
-class actores:
-    def __init__(self, id, nombre):
-        self.id=id
-        self.nombre=nombre
-    def __str__(self):
-        return f"{self.nombre}"
