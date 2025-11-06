3674386263a883a73ff232c195c491226928d798
SilverIdk <136312726+SilverIdk@users.noreply.github.com>
2025-11-05 18:00:13 -0700

    Update app.py

--- Unidad 3 IA/Proyecto_Sistema_Experto/app.py
+++ Unidad 3 IA/Proyecto_Sistema_Experto/app.py
@@ -1,6 +1,17 @@
+# Importa la libreria Tkinter para crear la ventana
 import tkinter as tk
+
+#Importa el modulo (archivo .py) que contiene la clase de la interfaz
 import Interfaz
+
+#Esto asegura que el codigo se ejecute cuando se ejecuta este archivo 
 if __name__ == "__main__":
+    
+    #Crea la ventana principal de la aplicacion
     ventana=tk.Tk()
+    
+    #Crea una instancia de la clase LoginInterfaz  y le pasa la ventana principal
     app = Interfaz.LoginInterfaz(ventana)
+
+    #Inicia el bucle principal de la aplicacion para que la ventana se mantenga abierta, esperando la interaccion del usuarrio hasta que se cierre
     ventana.mainloop()
